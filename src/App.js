83f8f7bd514d428461d606223cd8e9fb08be1393
王露 <1805118471@qq.com>
2018-12-08 11:38:26 +0800

    update app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Users from './components/ReactClassVSFuncComponents/Users';
+// import Users from './components/ReactClassVSFuncComponents/Users';
+import ReactEventAndTwoWayDataBinding from './components/ReactEventAndTwoWayDataBinding/ReactEventAndTwoWayDataBinding';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Users title="this is Users title from app" />
+        {/*<Users title="this is Users title from app" />*/}
+        <ReactEventAndTwoWayDataBinding />
       </div>
     )
   }
